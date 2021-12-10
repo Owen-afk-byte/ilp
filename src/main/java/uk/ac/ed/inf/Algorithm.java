ff65a3044910a42fe348e1e9fcc06d69ec258472
Owen-afk-byte <74568034+Owen-afk-byte@users.noreply.github.com>
2021-12-10 12:34:39 +0000

    pseudocode

--- src/main/java/uk/ac/ed/inf/Algorithm.java
+++ src/main/java/uk/ac/ed/inf/Algorithm.java
@@ -44,8 +44,7 @@ public class Algorithm {
      * @return a string which represents the GeoJSON of the drone flight path and is given back to the main class
      */
     public String mainAlgorithm() {
-
-
+        
         int numberDelivered = 0;
 
         LongLat nextMove = null;
@@ -75,18 +74,20 @@ public class Algorithm {
 
         try {
 
-            // read in everything
+            // reads in order and orderDetails tables
             Orders orders = new Orders(secondPort);
             ArrayList<ArrayList<String>> date = orders.getDates(year, month, day);
             HashMap<String, ArrayList<String>> details = orders.getDetails(date);
 
-
+            // reads in from the menus folder
             Menus menus = new Menus(firstPort);
 
+            // reads in buildings and landmarks from the menus folder
             Buildings buildings = new Buildings(firstPort);
             ArrayList<ArrayList<LongLat>> buildingCoordinates = buildings.getBuildings();
             ArrayList<LongLat> landmarkCoordinates = buildings.getLandmarks();
 
+            // reads in from the words folder
             Words words = new Words(firstPort);
 
             //puts orders in hash table and LongLats in array
@@ -118,6 +119,7 @@ public class Algorithm {
                 allPickups.put(entry.getKey(), pickupsArray);
             }
 
+            // one appleton has been reached everything is then added to the deliveries and flightpath tables
             if (appletonReached == true){
                 String jdbcString = "jdbc:derby://localhost:" + secondPort + "/derbyDB";
                 Connection conn = DriverManager.getConnection(jdbcString);
@@ -165,7 +167,9 @@ public class Algorithm {
                 System.out.println(numberOfMoves);
                 System.out.println(allPickups.size());
                 if (numberOfMoves == 0) {
+                    //prevent null pointer exception by setting start location to appleton for the first move
                     startLocation = appleton;
+                    // makes sure the drone returns to appleton after it exceeds 1450 moves
                 } else if (allDelivered == true || numberOfMoves > 1450) {
                     returnToAppleton = true;
                 } else {
@@ -223,6 +227,7 @@ public class Algorithm {
                             }
                         }
 
+                        // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                         nextMove = startLocation.nextPosition(firstMove);
                         dronePath.add(nextMove);
                         ArrayList<Double> dronePathArray = new ArrayList<>();
@@ -239,7 +244,10 @@ public class Algorithm {
                 }
                 else if (currentlyDelivering == false && pickingUp == false && headingLandmark == false) {
 
+                    // this is where the drone decides upon its next move, whether it will be delivering or picking up
 
+                    // this next chunk of code is used to create a list of the closeness for each pickup and its associated delivery divided by the cost
+                    // this allows the drone to go after the most cost effective option
                     HashMap<String, Double> jointCloseness = new HashMap<>();
                     for (Map.Entry<String, ArrayList<LongLat>> entry : allPickups.entrySet()) {
                         ArrayList<LongLat> pickupsArray = entry.getValue();
@@ -285,6 +293,7 @@ public class Algorithm {
                     Set<LongLat> set = new HashSet<>(items);
                     items.clear();
                     items.addAll(set);
+                    //once the pickups are made they are removed as so to not be used again when finding the closest pickups the next time
                     allPickups.remove(pickupID);
 
                     if (items.size() == 2) {
@@ -318,6 +327,7 @@ public class Algorithm {
                     }
                     nextMove = startLocation.nextPosition(finalMove);
 
+                    // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                     dronePath.add(nextMove);
                     ArrayList<Double> dronePathArray = new ArrayList<>();
                     dronePathArray.add(startLocation.longitude);
@@ -337,6 +347,7 @@ public class Algorithm {
 
                     if (startLocation.closeTo(pickupOne)){
 
+                        // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                         int angle = -999;
                         System.out.println(angle);
                         nextMove = startLocation.nextPosition(angle);
@@ -351,6 +362,8 @@ public class Algorithm {
                         justOrder.add(pickupID);
                         numberOfMoves = numberOfMoves  +1;
 
+                        // after delivering one item(s) the set in the pickupTwo variable will move into variable pickupOne
+                        // this allows for pickupOne to be the only variable used for searching for the items
                         pickupOne = pickupTwo;
                         pickupTwo = null;
                         if (pickupOne==null && pickupTwo==null){
@@ -375,6 +388,9 @@ public class Algorithm {
                                 finalMove = angleMove;
                             }
                         }
+
+                        // this is a check to ensure that the next move is not a duplicate of the last few
+                        // if it is then it reverts a few moves and heads for a landmark to unstick itself
                         LongLat checkMove = startLocation.nextPosition(finalMove);
                         double checkMoveLong = checkMove.longitude;
                         double checkMoveLat = checkMove.latitude;
@@ -382,21 +398,11 @@ public class Algorithm {
                             if ((dronePath.get(dronePath.size() - 5).longitude == checkMoveLong && dronePath.get(dronePath.size() - 5).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 4).longitude == checkMoveLong && dronePath.get(dronePath.size() - 4).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 3).longitude == checkMoveLong && dronePath.get(dronePath.size() - 3).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 2).longitude == checkMoveLong && dronePath.get(dronePath.size() - 2).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 1).longitude == checkMoveLong && dronePath.get(dronePath.size() - 1).latitude == checkMoveLat)) {
                                 doNotEnter.add(dronePath.get(dronePath.size()-1));
                                 numberOfMoves = numberOfMoves - 5;
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
+                                for (int i = 0; i < 5; i++) {
+                                    dronePath.remove(dronePath.size()-1);
+                                    justPath.remove(justPath.size()-1);
+                                    justOrder.remove(justOrder.size()-1);
+                                }
                                 startLocation = (dronePath.get(dronePath.size()-1));
                                 landmark = closestLandmark(startLocation, landmarkCoordinates);
                                 pickingUp = false;
@@ -421,6 +427,7 @@ public class Algorithm {
                             }
                         }
 
+                        // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                         nextMove = startLocation.nextPosition(finalMove);
                         dronePath.add(nextMove);
                         ArrayList<Double> dronePathArray = new ArrayList<>();
@@ -450,6 +457,7 @@ public class Algorithm {
                         deliveriesList.add(String.valueOf(totalCost));
                         deliveriesHashMap.put(pickupID, deliveriesList);
 
+                        // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                         int angle = -999;
                         nextMove = startLocation.nextPosition(angle);
                         dronePath.add(nextMove);
@@ -465,6 +473,8 @@ public class Algorithm {
 
                         currentlyDelivering = false;
 
+                        // checks to see if all of the deliveries have been made
+                        // if this is the case then allDelivered will be set to true and the drone will head back to Appleton
                         if (dateCoordinates.isEmpty()) {
                             allDelivered = true;
                         }
@@ -484,6 +494,9 @@ public class Algorithm {
                                 finalMove = angleMove;
                             }
                         }
+
+                        // this is a check to ensure that the next move is not a duplicate of the last few
+                        // if it is then it reverts a few moves and heads for a landmark to unstick itself
                         LongLat checkMove = startLocation.nextPosition(finalMove);
                         double checkMoveLong = checkMove.longitude;
                         double checkMoveLat = checkMove.latitude;
@@ -491,21 +504,11 @@ public class Algorithm {
                             if ((dronePath.get(dronePath.size() - 5).longitude == checkMoveLong && dronePath.get(dronePath.size() - 5).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 4).longitude == checkMoveLong && dronePath.get(dronePath.size() - 4).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 3).longitude == checkMoveLong && dronePath.get(dronePath.size() - 3).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 2).longitude == checkMoveLong && dronePath.get(dronePath.size() - 2).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 1).longitude == checkMoveLong && dronePath.get(dronePath.size() - 1).latitude == checkMoveLat)) {
                                 doNotEnter.add(dronePath.get(dronePath.size()-1));
                                 numberOfMoves = numberOfMoves - 5;
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
+                                for (int i = 0; i < 5; i++) {
+                                    dronePath.remove(dronePath.size()-1);
+                                    justPath.remove(justPath.size()-1);
+                                    justOrder.remove(justOrder.size()-1);
+                                }
                                 startLocation = (dronePath.get(dronePath.size()-1));
                                 landmark = closestLandmark(startLocation, landmarkCoordinates);
                                 currentlyDelivering = false;
@@ -528,6 +531,7 @@ public class Algorithm {
                             }
                         }
 
+                        // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                         nextMove = startLocation.nextPosition(finalMove);
                         dronePath.add(nextMove);
                         ArrayList<Double> dronePathArray = new ArrayList<>();
@@ -545,6 +549,8 @@ public class Algorithm {
                     }
                 }else if (currentlyDelivering == false && pickingUp == false && headingLandmark == true) {
                     startLocation = nextMove;
+                    // this prevents the drone from heading towards a landmark for more than 15 moves
+                    // 15 was decided because it seems to be the most effective at unsticking the drone without using too many moves
                     if (landmarkMoves < 15) {
 
                         moves = moves(startLocation, landmark, buildingCoordinates);
@@ -558,6 +564,9 @@ public class Algorithm {
                                 finalMove = angleMove;
                             }
                         }
+
+                        // this is a check to ensure that the next move is not a duplicate of the last few
+                        // if it is then it reverts a few moves and heads for a landmark to unstick itself
                         LongLat checkMove = startLocation.nextPosition(finalMove);
                         double checkMoveLong = checkMove.longitude;
                         double checkMoveLat = checkMove.latitude;
@@ -565,21 +574,13 @@ public class Algorithm {
                             if ((dronePath.get(dronePath.size() - 3).longitude == checkMoveLong && dronePath.get(dronePath.size() - 3).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 2).longitude == checkMoveLong && dronePath.get(dronePath.size() - 2).latitude == checkMoveLat) || (dronePath.get(dronePath.size() - 1).longitude == checkMoveLong && dronePath.get(dronePath.size() - 1).latitude == checkMoveLat)) {
                                 doNotEnter.add(dronePath.get(dronePath.size()-1));
                                 numberOfMoves = numberOfMoves - 3;
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-                                dronePath.remove(dronePath.size()-1);
-
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-                                justPath.remove(justPath.size()-1);
-
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-                                justOrder.remove(justOrder.size()-1);
-
+                                for (int i = 0; i < 3; i++) {
+                                    dronePath.remove(dronePath.size()-1);
+                                    justPath.remove(justPath.size()-1);
+                                    justOrder.remove(justOrder.size()-1);
+                                }
                                 startLocation = (dronePath.get(dronePath.size()-1));
                                 landmark = closestLandmark(startLocation, landmarkCoordinates);
-
                             }
                         }
 
@@ -598,6 +599,7 @@ public class Algorithm {
 
                         nextMove = startLocation.nextPosition(finalMove);
 
+                        // collates all of the necessary data about the drones actions to later be written to the database and GeoJSON
                         dronePath.add(nextMove);
                         ArrayList<Double> dronePathArray = new ArrayList<>();
                         dronePathArray.add(startLocation.longitude);
@@ -610,6 +612,8 @@ public class Algorithm {
                         numberOfMoves = numberOfMoves + 1;
                         landmarkMoves = landmarkMoves + 1;
                     }else{
+                        // returns the drone back to its initial state prior ro heading towards a landmark
+                        // means that drone can continue to pickup or deliver what it had intended
                         landmarkMoves = 0;
                         headingLandmark = false;
                         if (pausedDelivery == true){
@@ -633,6 +637,8 @@ public class Algorithm {
             System.exit(1); // Exit the application
         }
 
+        // completes drone path
+        // this was necessary to add the move from appleton to the next position of the drone
         ArrayList<LongLat> finalDronePath = new ArrayList<>();
         finalDronePath.add(appleton);
         finalDronePath.addAll(dronePath);
@@ -647,6 +653,7 @@ public class Algorithm {
             noPoints = noPoints +1;
         }
 
+        // converts the drone path into a GeoJSON string to be outputed to the main class in the App class
         System.out.println("noPoints " + noPoints);
         LineString lineString = LineString.fromLngLats(points);
         Geometry geometry = (Geometry) lineString;
@@ -674,6 +681,7 @@ public class Algorithm {
             currentLandmark = secondLandmark;
         }
 
+        // if the drone is still looping while travelling towards a landmark it will change which landmark that it is heading towards
         if (dronePath.size()>3) {
             if ((dronePath.get(dronePath.size() - 3).longitude == startLocation.longitude && dronePath.get(dronePath.size() - 3).latitude == startLocation.latitude) || (dronePath.get(dronePath.size() - 2).longitude == startLocation.longitude && dronePath.get(dronePath.size() - 2).latitude == startLocation.latitude) || (dronePath.get(dronePath.size() - 1).longitude == startLocation.longitude && dronePath.get(dronePath.size() - 1).latitude == startLocation.latitude) ) {
                 if (currentLandmark == firstLandmark){
@@ -744,6 +752,9 @@ public class Algorithm {
                 noEntry = false;
             }
         }
+
+        // checks that the entered move is safe
+        // the move has to be within the confined zone, not flying over no-fly zones, and not entering coordinates from the array doNotEnter
         if (canMove == true && nextMove.isConfined() == true && noEntry == false) {
             moveOkay = true;
         } else {
@@ -764,6 +775,7 @@ public class Algorithm {
 
         HashMap<Integer, Double> tempAngles = new HashMap<>();
 
+        // all 36 directions are tested hear to make sure that the drone is finding the best possible route
         for (int a = 0; a < 36; a++) {
             int angle = a * 10;
             boolean checkSafe = safeMove(angle, startLocation, buildingCoordinates);
